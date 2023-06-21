44eb152409a06034361c672b00cb8929667e2e40
bapHyv <baptiste.hyver@gmail.com>
2023-06-21 17:02:37 +0200

    commit

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink, split } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { UserProvider } from './contexts/UserContext';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { createClient } from 'graphql-ws';
+import { getMainDefinition } from '@apollo/client/utilities';
+
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: 'ws://localhost:5000/graphql',
+    connectionParams: {
+      authentication: localStorage.getItem('token'),
+    },
+  }),
+);
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:5000/graphql',
@@ -22,9 +34,17 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
+  },
+  wsLink,
+  httpLink,
+);
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  // authLink.concat(httpLink),
+  link: authLink.concat(splitLink),
   cache: new InMemoryCache(),
 });
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
